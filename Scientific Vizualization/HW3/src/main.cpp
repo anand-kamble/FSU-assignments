1b2495c22d178069552b2686d65badf14271d6ea
anand-kamble <anandmk837@gmail.com>
2024-02-19 03:37:17 +0530

    Removed all the comments

--- Scientific Vizualization/HW3/src/main.cpp
+++ Scientific Vizualization/HW3/src/main.cpp
@@ -15,9 +15,12 @@ using namespace glm;
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
-// settings
+
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+
+
 
 float LowerArmAngle = 20.0f;
 float UpperArmAngle = 40.0f;
@@ -26,8 +29,8 @@ float cameraSpeedFactor = 0.0f;
 
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
+    
+    
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -37,8 +40,8 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    // glfw window creation
-    // --------------------
+    
+    
     GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Anand", NULL, NULL);
     if (window == NULL)
     {
@@ -49,24 +52,24 @@ int main()
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
+    
+    
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // configure global opengl state
-    // -----------------------------
+    
+    
     glEnable(GL_DEPTH_TEST);
 
-    // build and compile our shader zprogram
-    // ------------------------------------
+    
+    
     Shader ourShader("src/Shader/shader.vert", "src/Shader/shader.frag");
 
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
+    
+    
     float vertices[] = {
         -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
         0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
@@ -119,14 +122,14 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // position attribute
+    
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
-    // texture coord attribute
+    
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    // load and create a texture
+    
     unsigned int texture1;
     glGenTextures(1, &texture1);
     glBindTexture(GL_TEXTURE_2D, texture1);
@@ -135,7 +138,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     int width, height, nrChannels;
-    stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+    stbi_set_flip_vertically_on_load(true); 
     unsigned char *data = stbi_load("src/texture/aluminium.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
@@ -157,20 +160,20 @@ int main()
     {
         processInput(window);
 
-        // render
-        // ------
+        
+        
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // bind textures on corresponding texture units
+        
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
 
-        // activate shader
+        
         ourShader.use();
 
-        // camera/view transformation
-        mat4 view = mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        
+        mat4 view = mat4(1.0f); 
         float radius = 10.0f;
 
         float camX = static_cast<float>(sin(cameraSpeedFactor) * radius);
@@ -182,8 +185,8 @@ int main()
 
         auto ArmScale = vec3(2.0f, 0.4f, 0.4f);
         vec3 armPosition = vec3(0.0f, 0.0f, 0.0f);
-        // Lower Arm
-        // ------------------------------------------------------------------
+        
+        
         auto model = mat4(1.0f);
 
         model = rotate(model, radians(LowerArmAngle), vec3(0.0f, 0.0f, 1.0f));
@@ -197,7 +200,7 @@ int main()
         ourShader.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
-        // Upper Arm
+        
         model = mat4(1.0f);
         model = rotate(model, radians(LowerArmAngle), vec3(0.0f, 0.0f, 1.0f));
         model = translate(model, vec3(1.0f, 0.0f, 0.0f));
@@ -209,7 +212,7 @@ int main()
         ourShader.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
-        // Finger 1
+        
         model = mat4(1.0f);
         model = rotate(model, radians(LowerArmAngle), vec3(0.0f, 0.0f, 1.0f));
         model = translate(model, vec3(1.0f, 0.0f, 0.0f));
@@ -224,7 +227,7 @@ int main()
         ourShader.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
-        // Finger 2
+        
         model = mat4(1.0f);
         model = rotate(model, radians(LowerArmAngle), vec3(0.0f, 0.0f, 1.0f));
         model = translate(model, vec3(1.0f, 0.0f, 0.0f));
@@ -239,25 +242,25 @@ int main()
         ourShader.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
+        
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
+    
+    
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
 
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
+    
+    
     glfwTerminate();
     return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
+
+
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -274,11 +277,11 @@ void processInput(GLFWwindow *window)
         cameraSpeedFactor += 0.01f;
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
+
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and
-    // height will be significantly larger than specified on retina displays.
+    
+    
     glViewport(0, 0, width, height);
 }
