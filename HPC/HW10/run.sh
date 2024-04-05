0034bc04aa30109d31031912304361a24348006a
anand-kamble <amk23j@fsu.edu>
2024-04-04 20:56:46 -0400

    Code of HW10 of HPC is done

---
+++ HPC/HW10/run.sh
@@ -0,0 +1,14 @@
+# I have made this script for classroom machines
+# And I have tested it on the class13 machine.
+
+module load hpc_sdk/nvhpc-hpcx-cuda12/24.3
+
+make clean
+make
+
+echo "Running the K-Means CUDA program..."
+
+# Run the program with k = 7
+echo "7" | ./bin/test.x
+#     ^
+#     | Change this value to change the number of clusters
--- HPC/HW10/src/main.cu
+++ HPC/HW10/src/main.cu
@@ -1,18 +1,23 @@
 #include <cuda_runtime.h>
-#include <device_launch_parameters.h>
-#include <iostream>
 #include <cmath>
-#include <fstream>
-#include <algorithm>
+#include <stdint.h>
+#include <iostream>
 #include "../includes/Jpegfile.h"
 
-struct PIXEL
+struct Pixel
 {
-    int x, y, z;
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
 };
 
-// Kernel for grouping pixels to nearest generator
-__global__ void groupingKernel(UINT width, UINT height, int k, PIXEL *colors, PIXEL *generators, int *groupColorSum, int *groupCount)
+double euclideanDistance(const Pixel *p1, const Pixel *p2)
+{
+    double sum = 0.0;
+    return sqrt(pow(p1->r - p2->r, 2) + pow(p1->g - p2->g, 2) + pow(p1->b - p2->b, 2));
+};
+
+__global__ void groupingKernel(UINT width, UINT height, int k, Pixel *colors, Pixel *generators, int *groupColorSum, int *groupCount)
 {
     int x = threadIdx.x + blockIdx.x * blockDim.x;
     int y = threadIdx.y + blockIdx.y * blockDim.y;
@@ -20,15 +25,15 @@ __global__ void groupingKernel(UINT width, UINT height, int k, PIXEL *colors, PI
     if (x < width && y < height)
     {
         int index = y * width + x;
-        PIXEL pixel = colors[index];
+        Pixel pixel = colors[index];
 
         float minDist = INFINITY;
         int minIndex = 0;
 
         for (int i = 0; i < k; i++)
         {
-            PIXEL gen = generators[i];
-            float dist = sqrtf((pixel.x - gen.x) * (pixel.x - gen.x) + (pixel.y - gen.y) * (pixel.y - gen.y) + (pixel.z - gen.z) * (pixel.z - gen.z));
+            Pixel gen = generators[i];
+            float dist = sqrtf((pixel.r - gen.r) * (pixel.r - gen.r) + (pixel.g - gen.g) * (pixel.g - gen.g) + (pixel.b - gen.b) * (pixel.b - gen.b));
             if (dist < minDist)
             {
                 minDist = dist;
@@ -36,27 +41,25 @@ __global__ void groupingKernel(UINT width, UINT height, int k, PIXEL *colors, PI
             }
         }
 
-        atomicAdd(&groupColorSum[minIndex * 3 + 0], (int)pixel.x);
-        atomicAdd(&groupColorSum[minIndex * 3 + 1], (int)pixel.y);
-        atomicAdd(&groupColorSum[minIndex * 3 + 2], (int)pixel.z);
+        atomicAdd(&groupColorSum[minIndex * 3 + 0], (int)pixel.r);
+        atomicAdd(&groupColorSum[minIndex * 3 + 1], (int)pixel.g);
+        atomicAdd(&groupColorSum[minIndex * 3 + 2], (int)pixel.b);
         atomicAdd(&groupCount[minIndex], 1);
     }
 }
 
-// Kernel for updating generators
-__global__ void updateGeneratorsKernel(int k, PIXEL *generators, int *groupColorSum, int *groupCount)
+__global__ void updateGeneratorsKernel(int k, Pixel *generators, int *groupColorSum, int *groupCount)
 {
     int i = threadIdx.x;
     if (i < k)
     {
-        generators[i].x = (float)groupColorSum[i * 3 + 0] / groupCount[i];
-        generators[i].y = (float)groupColorSum[i * 3 + 1] / groupCount[i];
-        generators[i].z = (float)groupColorSum[i * 3 + 2] / groupCount[i];
+        generators[i].r = (int)groupColorSum[i * 3 + 0] / groupCount[i];
+        generators[i].g = (int)groupColorSum[i * 3 + 1] / groupCount[i];
+        generators[i].b = (int)groupColorSum[i * 3 + 2] / groupCount[i];
     }
 }
 
-// Kernel for replacing pixel colors with group generators
-__global__ void replaceColorKernel(UINT width, UINT height, int k, PIXEL *colors, PIXEL *generators, int *groupCount)
+__global__ void replaceColorKernel(UINT width, UINT height, int k, Pixel *colors, Pixel *generators, int *groupCount)
 {
     int x = threadIdx.x + blockIdx.x * blockDim.x;
     int y = threadIdx.y + blockIdx.y * blockDim.y;
@@ -64,15 +67,15 @@ __global__ void replaceColorKernel(UINT width, UINT height, int k, PIXEL *colors
     if (x < width && y < height)
     {
         int index = y * width + x;
-        PIXEL pixel = colors[index];
+        Pixel pixel = colors[index];
 
         float minDist = INFINITY;
         int minIndex = 0;
 
         for (int i = 0; i < k; i++)
         {
-            PIXEL gen = generators[i];
-            float dist = sqrtf((pixel.x - gen.x) * (pixel.x - gen.x) + (pixel.y - gen.y) * (pixel.y - gen.y) + (pixel.z - gen.z) * (pixel.z - gen.z));
+            Pixel gen = generators[i];
+            float dist = sqrtf((pixel.r - gen.r) * (pixel.r - gen.r) + (pixel.g - gen.g) * (pixel.g - gen.g) + (pixel.b - gen.b) * (pixel.b - gen.b));
             if (dist < minDist)
             {
                 minDist = dist;
@@ -86,72 +89,72 @@ __global__ void replaceColorKernel(UINT width, UINT height, int k, PIXEL *colors
 
 int main()
 {
-    // 1. Host reads the jpg file
     UINT width, height;
     uint8_t *hostDataBuf = JpegFile::JpegFileToRGB("test-large.jpg", &width, &height);
 
-    // 2. Allocate memory in Device, copy colors from Host to Device
-    PIXEL *d_colors;
-    cudaMalloc(&d_colors, width * height * sizeof(PIXEL));
-    cudaMemcpy(d_colors, hostDataBuf, width * height * sizeof(PIXEL), cudaMemcpyHostToDevice);
+    const int N = height * width; // Total number of pixels
 
-    // 3. Host generates k generates, allocate Device memory for generators, and copy the generates from Host to Device
-    int k = 8;
-    auto generators = new PIXEL[k];
+    Pixel *device_colors;
+
+    cudaMalloc(&device_colors, width * height * sizeof(Pixel));
+    cudaMemcpy(device_colors, hostDataBuf, width * height * sizeof(Pixel), cudaMemcpyHostToDevice);
+
+    int k;
+    std::cout << "Enter the number of clusters: ";
+    std::cin >> k;
+
+    auto generators = new Pixel[k];
+    printf("Generators: \n");
     for (int i = 0; i < k; i++)
     {
-        generators[i].x = hostDataBuf[i * 3];
-        generators[i].y = hostDataBuf[i * 3 + 1];
-        generators[i].z = hostDataBuf[i * 3 + 2];
+        auto G = generators[i];
+        G.r = hostDataBuf[(N * 3 / k) * i];
+        G.g = hostDataBuf[(N * 3 / k) * i + 1];
+        G.b = hostDataBuf[(N * 3 / k) * i + 2];
+        printf("Generator %d: %d %d %d\n", i, G.r, G.g, G.b);
     }
-    PIXEL *d_generators;
-    cudaMalloc(&d_generators, k * sizeof(PIXEL));
-    cudaMemcpy(d_generators, generators, k * sizeof(PIXEL), cudaMemcpyHostToDevice);
 
-    // 4. Allocate Device memory for each group color sum, group count, and initialize it to zero
-    int *d_groupColorSum;
-    int *d_groupCount;
-    cudaMalloc(&d_groupColorSum, k * 3 * sizeof(int));
-    cudaMalloc(&d_groupCount, k * sizeof(int));
-    cudaMemset(d_groupColorSum, 0, k * 3 * sizeof(int));
-    cudaMemset(d_groupCount, 0, k * sizeof(int));
+    Pixel *device_generators;
+    cudaMalloc(&device_generators, k * sizeof(Pixel));
+    cudaMemcpy(device_generators, generators, k * sizeof(Pixel), cudaMemcpyHostToDevice);
+
+    int *device_groupColorSum;
+    int *device_groupCount;
+    cudaMalloc(&device_groupColorSum, k * 3 * sizeof(int));
+    cudaMalloc(&device_groupCount, k * sizeof(int));
+    cudaMemset(device_groupColorSum, 0, k * 3 * sizeof(int));
+    cudaMemset(device_groupCount, 0, k * sizeof(int));
 
     dim3 blockSize(16, 16);
     dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y);
-
-    // 5. Start of iterations from Host
     for (int iter = 0; iter < 10; iter++)
     {
         // 6. Launch the grouping kernel
-
-        groupingKernel<<<gridSize, blockSize>>>(width, height, k, d_colors, d_generators, d_groupColorSum, d_groupCount);
+        groupingKernel<<<gridSize, blockSize>>>(width, height, k, device_colors, device_generators, device_groupColorSum, device_groupCount);
         cudaDeviceSynchronize();
 
         // 7. Launch the update generators kernel
-        updateGeneratorsKernel<<<1, k>>>(k, d_generators, d_groupColorSum, d_groupCount);
+        updateGeneratorsKernel<<<1, k>>>(k, device_generators, device_groupColorSum, device_groupCount);
         cudaDeviceSynchronize();
 
         // Reset group color sum and count
-        cudaMemset(d_groupColorSum, 0, k * 3 * sizeof(int));
-        cudaMemset(d_groupCount, 0, k * sizeof(int));
+        cudaMemset(device_groupColorSum, 0, k * 3 * sizeof(int));
+        cudaMemset(device_groupCount, 0, k * sizeof(int));
     }
 
-    // 9. Launch the replace color kernel
-    replaceColorKernel<<<gridSize, blockSize>>>(width, height, k, d_colors, d_generators, d_groupCount);
+    replaceColorKernel<<<gridSize, blockSize>>>(width, height, k, device_colors, device_generators, device_groupCount);
     cudaDeviceSynchronize();
 
-    // 10. Copy colors from Device back to Host
-    cudaMemcpy(hostDataBuf, d_colors, width * height * sizeof(PIXEL), cudaMemcpyDeviceToHost);
+    cudaMemcpy(hostDataBuf, device_colors, N * sizeof(Pixel), cudaMemcpyDeviceToHost);
 
-    // 11. Host write the gray image to a jpg file
     JpegFile::RGBToJpegFile("output.jpg", hostDataBuf, width, height, 100, false);
 
-    // 12. Free memory
-    cudaFree(d_colors);
-    cudaFree(d_generators);
-    cudaFree(d_groupColorSum);
-    cudaFree(d_groupCount);
-    delete[] hostDataBuf;
+    delete[] generators;
+    free(hostDataBuf);
+    cudaFree(device_colors);
+    cudaFree(device_generators);
+    cudaFree(device_groupColorSum);
+    cudaFree(device_groupCount);
 
     return 0;
 }
