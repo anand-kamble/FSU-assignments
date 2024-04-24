6b9f1ab52010c98c15a2bb73f663977d4b6ad273
anand-kamble <amk23j@fsu.edu>
2024-04-24 14:26:43 -0400

    Rendered all the videos

--- Scientific Vizualization/Project/make_video.sh
+++ Scientific Vizualization/Project/make_video.sh
@@ -1,1 +1,5 @@
-ffmpeg -framerate 15 -pattern_type glob -i 'reflec_pres/movie-0/*.png' -c:a copy -shortest -c:v libx264 -pix_fmt yuv420p out.mp4
+ffmpeg -framerate 15 -pattern_type glob -i 'reflec_pres/movie-0/*.png' -c:a copy -shortest -c:v libx264 -pix_fmt yuv420p pres_out.mp4 &&\
+ffmpeg -framerate 15 -pattern_type glob -i 'reflec_temp/movie-0/*.png' -c:a copy -shortest -c:v libx264 -pix_fmt yuv420p temp0_out.mp4 &&\
+ffmpeg -framerate 15 -pattern_type glob -i 'reflec_temp/movie-1/*.png' -c:a copy -shortest -c:v libx264 -pix_fmt yuv420p temp1_out.mp4 &&\
+ffmpeg -framerate 15 -pattern_type glob -i 'reflec_ener/movie-0/*.png' -c:a copy -shortest -c:v libx264 -pix_fmt yuv420p ener_out.mp4 &&\
+ffmpeg -framerate 15 -pattern_type glob -i 'reflec_dens/movie-0/*.png' -c:a copy -shortest -c:v libx264 -pix_fmt yuv420p dens_out.mp4
